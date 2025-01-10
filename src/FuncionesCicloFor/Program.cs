36d8b4bec543d67e93a97cec51d821d4e69ace8e
r-chamorro <roger.a.chamorro@hotmail.com>
2025-01-10 10:36:36 -0600

    [UPDATE] - Terminando ejercicios
    Completados ejercicios practicos con While, do while y for.

--- src/FuncionesCicloFor/Program.cs
+++ src/FuncionesCicloFor/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FuncionesCicloFor
 {
@@ -10,6 +7,38 @@ namespace FuncionesCicloFor
     {
         static void Main(string[] args)
         {
+            /*
+            4 - Realice un programa en C# que muestre por pantalla las siguientes funciones: f(x) = x, f(x)
+            = 2x, f(x) = x2, f(x) = √x. El programa mostrará las imágenes de dichas funciones para todos
+            los valores de x comprendidos entre 1 y un número N dado por el usuario. Por ejemplo,
+            para N = 6, el programa mostrará
+             */
+
+            int numero;
+            double pordos, cuadrado, raiz;
+
+            Console.Title = "Funciones Matematicas";
+            Console.WriteLine("operaciones Matematicas!");
+            Console.WriteLine("Ingrese un Numero");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0,10} | {1,10} | {2,10} | {3,10}","f(x) = x","f(x) = 2x","f(x) = x^2", "f(x) = √x");
+            Console.WriteLine("--------------------------------------------------");
+
+            for (int i = 1; i <= numero; i++)
+            {
+
+                pordos = i * 2;
+                cuadrado = Math.Round(Math.Pow(i, 2),2);
+                raiz =Math.Round(Math.Sqrt(i),2);
+                Console.WriteLine("{0,10} | {1,10} | {2,10} | {3,10}", i,pordos,cuadrado,raiz);
+
+                
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
