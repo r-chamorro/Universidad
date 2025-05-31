94258b21ab8db54eba41efe2c3b12db1f0f3d8bf
r-chamorro <roger.a.chamorro@hotmail.com>
2025-05-31 13:34:01 -0600

    [NEW] - Segundo Parcial
    Incluyendo ejercicios de segundo parcial de Algoritmos, ciclos y arreglos.

---
+++ src/Segundo/entregaGuia4/ejercicio7.cpp
@@ -0,0 +1,16 @@
+/*
+iii. Modificación
+Ejercicio 7:
+Crear un programa que permita buscar un número en un arreglo y modificarlo por un nuevo valor ingresado por el
+usuario.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    
+
+    return 0;
+}
